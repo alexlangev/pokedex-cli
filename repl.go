8605ad67a5c9fe0b17476031a6a256efb3f93c71
Alex Langevin <alexlangev@gmail.com>
2026-03-30 21:06:58 -0400

    map forwards and backwards

--- repl.go
+++ repl.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -16,14 +19,15 @@ func cleanInput(text string) []string {
 	return cleanWords
 }
 
-// infinite loop until exit
-// prints on screen
-// reads stdin
-// repeat
 func repl() {
 	const prompt = "Pokedex > "
 
 	scanner := bufio.NewScanner(os.Stdin)
+
+	config := config{
+		prevURL: "",
+		nextURL: "https://pokeapi.co/api/v2/location-area/?offset=0",
+	}
 
 	for {
 		fmt.Print(prompt)
@@ -33,19 +37,31 @@ func repl() {
 		userInput := scanner.Text()
 		args := cleanInput(userInput)
 
+		if len(args) == 0 {
+			continue
+		}
+
 		// does command exist? is so call the callback
 		if cmd, ok := commands[args[0]]; ok {
-			cmd.callback()
+			cmd.callback(&config)
 		} else {
 			fmt.Println("Unknown command")
 		}
+
+		fmt.Println("        ")
+		fmt.Println("next: ", config.nextURL)
 	}
 }
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	nextURL string
+	prevURL string
 }
 
 var commands = map[string]cliCommand{
@@ -59,17 +75,108 @@ var commands = map[string]cliCommand{
 		description: "displays a help message",
 		callback:    commandHelp,
 	},
+	"map": {
+		name:        "map",
+		description: "display the next 20 areas",
+		callback:    commandMap,
+	},
+	"mapb": {
+		name:        "mapb",
+		description: "display the previous 20 areas",
+		callback:    commandMapb,
+	},
 }
 
-func commandExit() error {
+func commandExit(*config) error {
 	const msg = "Closing the Pokedex... Goodbye!"
 	fmt.Println(msg)
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(*config) error {
 	const msg = "Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex"
 	fmt.Println(msg)
 	return nil
+}
+
+func commandMap(c *config) error {
+	if c.nextURL == "" {
+		fmt.Println("Already at the end")
+		return nil
+	}
+
+	res, err := http.Get(c.nextURL)
+	if err != nil {
+		return err
+	}
+
+	// read response
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	// unmarshal
+	var data locationAreas
+	if err := json.Unmarshal(body, &data); err != nil {
+		return err
+	}
+
+	// update config with new next and prev
+	c.prevURL = data.Previous
+	c.nextURL = data.Next
+
+	// print location areas
+	for _, loc := range data.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.prevURL == "" {
+		fmt.Println("Already at the start")
+		return nil
+	}
+	res, err := http.Get(c.prevURL)
+	if err != nil {
+		return err
+	}
+
+	// read response
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return err
+	}
+
+	// unmarshal
+	var data locationAreas
+	if err := json.Unmarshal(body, &data); err != nil {
+		return err
+	}
+
+	// update config with new next and prev
+	c.prevURL = data.Previous
+	c.nextURL = data.Next
+
+	// print location areas
+	for _, loc := range data.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
+
+type locationAreas struct {
+	Count    int            `json:"count"`
+	Next     string         `json:"next"`
+	Previous string         `json:"previous"`
+	Results  []locationArea `json:"results"`
+}
+
+type locationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
